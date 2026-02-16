86dddf6c5bd1f3684db58f187d4ab7dadc3981b6
AsciiMoth <ascii@moth.contact>
2026-02-16 09:19:52 +0400

    chore: setup nix devenv

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "rs3a";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            commitizen.enable = true;
+            typos.enable = true;
+            typos-commit = {
+              enable = true;
+              description = "Find typos in commit message";
+              entry = let script = pkgs.writeShellScript "typos-commit" ''
+                typos "$1"
+              ''; in builtins.toString script;
+              stages = [ "commit-msg" ];
+            };
+          };
+        };
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = with pkgs; [
+          cargo
+
+          typos
+        ];
+      };
+    });
+}
